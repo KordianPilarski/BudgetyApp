5e707a979b722bae98ab37ac3bd7b7b88b55b9be
The-Krd <kaerde91@gmail.com>
2019-06-18 09:52:33 +0200

    add getting values from inputs

--- app.js
+++ app.js
@@ -8,18 +8,39 @@ var budgetController = (function(){
 //UI CNTRLR
 var UIController = (function(){
 
-    // some code
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,  //inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    };
 
 })();
 
 //GLOBAL APP CNTRLR
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UIController.getDOMStrings();
+
     var ctrlAddItem = function () {
 
         //get value of add desc
-        var value = document.querySelector('.add__value').value
-        return value;
+        var input = UICtrl.getInput();
+        console.log(input);
         //add item to budget
 
         //add item to UI
@@ -27,17 +48,15 @@ var controller = (function(budgetCtrl, UICtrl){
         // calc budget
 
         // display budget to UI
-        console.log('werkbicz')
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
 
         if (event.keyCode === 13 || event.which === 13){
            ctrlAddItem();
         }
-
     });
 
 })(budgetController, UIController);
